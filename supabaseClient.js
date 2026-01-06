d74d97802857060e3ca4e632b4ad65510f5a4704
hicham566 <hichamstitou1@gmail.com>
2026-01-06 01:40:43 +0000

    commit supabase

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // TODO: User to replace these with their own project details from supabase.com
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co'
-const SUPABASE_KEY = 'YOUR_ANON_KEY'
+const SUPABASE_URL = 'https://ziysrqhppmibfgqyyrqx.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_3LeWsGoWPMHlrX7SK-_x2Q_Ao5IgOyy'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
